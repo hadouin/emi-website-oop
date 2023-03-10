7f5aafbbd1435d10dc96aef6290ba17d76530e81
Hadouin Leroy <hadouinl@gmail.com>
2023-03-10 17:35:38 +0100

    dashboard chart.js

--- html/templates/app/dashboard.php
+++ html/templates/app/dashboard.php
@@ -1,8 +1,84 @@
 <?php $title = "Emi - App" ?>
 <?php ob_start(); ?>
     <!-- Main -->
-    <main class="container">
-        <p>Welcome to app</p>
+    <main>
+        <h1 style="margin-top: 0">Dashboard</h1>
+        <div class="grid">
+            <div>
+                <article>
+                    <canvas id="myChart"></canvas>
+                </article>
+            </div>
+            <div>
+                <article>
+                    <canvas id="myOtherChart"></canvas>
+                </article>
+            </div>
+        </div>
     </main><!-- ./ Main -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script defer>
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const data = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [{
+                axis: 'y',
+                label: 'My First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                ],
+                borderWidth: 1
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data,
+            options: {
+                indexAxis: 'y',
+            }
+        };
+        const otherChart = document.getElementById('myOtherChart');
+
+        new Chart(otherChart, config)
+    </script>
 <?php $content = ob_get_clean(); ?>
 <?php require('+layout.php') ?>
